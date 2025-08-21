88567b7cd45dd2a9bfc27c09f29d0376f0b2029a
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-08-21 14:05:09 -0700

    Create sign up form and validation middleware
    The sign up form re-renders the sign up form when an error with the
    username or passwords comes up, and supplies a message detailing what
    was wrong. It will also refill the non-password inputs.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,42 @@
 import {
   createUser
 } from "../db/userQueries.js";
+import { validationResult } from "express-validator";
 
 function newUserGet(req, res) {
   res.render("signUp", {
     url: "/sign-up",
+    firstName: "",
+    lastName: "",
+    username: "",
+    errors: null,
   });
 }
 
 async function newUserPost(req, res) {
-  // hash password before sending to DB
-  // await createUser();
-  res.redirect("/");
+  const { firstName, lastName, username } = req.body;
+  const result = validationResult(req);
+  console.log(result.array());
+  if (!result.isEmpty()) {
+    const formattedErrors = {};
+
+    for (const error of result.array()) {
+      formattedErrors[error.path] = error.msg;
+    }
+
+    res.status(400).render("signUp", {
+      url: "/sign-up",
+      firstName: firstName,
+      lastName: lastName,
+      username: username,
+      errors: formattedErrors,
+    });
+  } else {
+    // hash password before sending to DB
+    // await createUser();
+    res.redirect("/");
+  }
+  
 }
 
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,26 @@
 import { Router } from "express";
 import { newUserGet, newUserPost } from "../controllers/userController.js";
+import { body } from "express-validator";
 
 const userRouter = Router();
 
 userRouter.get("/", newUserGet);
-userRouter.post("/", newUserPost); // submitted sign up form
+userRouter.post(
+  "/",
+  body("username").custom(async (value) => {
+    const existingUsername = true; // TODO: find username
+    if (existingUsername) {
+      throw new Error("This username is unavailable.");
+    }
+    return true;
+  }),
+  body("confirmPassword").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Passwords do not match.");
+    }
+    return true;
+  }),
+  newUserPost
+);
 
 export default userRouter;
